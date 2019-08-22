ae6885cbbb5dc6dea20fcc6c90d091897aa717aa
jonah polack <mail@jonahpolack.de>
2019-08-22 14:43:57 +0200

    changes for heroku

--- src/server/index.js
+++ src/server/index.js
@@ -20,4 +20,7 @@ if (process.env.NODE_ENV !== 'production') {
   app.use('/', appBuilder);
 }
 
-app.listen(3000);
+const port = process.env.PORT || 3000;
+app.listen(port);
+
+console.log('Started on Port', port);
