38eb14f225974fdcdee30285bb403d6c7fab0058
jonah polack <mail@jonahpolack.de>
2019-08-22 15:05:09 +0200

    fixed dev dependencies

--- src/server/app.js
+++ src/server/app.js
@@ -1,9 +1,10 @@
-const webpack = require('webpack');
-const middleware = require('webpack-dev-middleware');
-const webpackConfig = require('../client/webpack.config');
-
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable global-require */
 
 module.exports = () => {
+  const webpack = require('webpack');
+  const middleware = require('webpack-dev-middleware');
+  const webpackConfig = require('../client/webpack.config');
   const compiler = webpack(webpackConfig);
   return middleware(compiler);
 };
